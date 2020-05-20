8f33f12543984312b954dcd2de225daef0e47a09
Caroline Ninganga <carolninganga@gmail.com>
2020-05-20 11:52:23 -0400

    added functions

--- db/app.js
+++ db/app.js
@@ -11,9 +11,63 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    StartRun();
+    //connection.end();
   });
 
+  const viewOptions = [
+    "ViewEmployees",
+    "ViewDepartment",
+    "ViewRoles",
+    "UpdateEmployee",
+    "exit"
+  ];
+
+  const UpdateOptions = [
+    "First Name",
+    "Last Name",
+    "Role",
+    "exit"
+  ]
+
+  // const CreateDepartmentOption = [
+  //   ["name"]
+  // ]
+
+  function start() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: viewOptions
+    })
+    .then(function(answer) {
+      switch(answer.action){
+        case viewOptions[0]:
+          employeeView();
+          break;
+
+        case viewOptions[1]:
+          departmentView();
+          break;
+
+        case viewOptions[3]:
+          roleView();
+          break;
+
+        case UpdateOptions[4]:
+          connection.end();
+      }
+    })
+  }
+
+
+  // * Add departments, roles, employees
+
+  // * View departments, roles, employees
+
+  // * Update employee roles
 
 //   function start() {
 //       inquirer
