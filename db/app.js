dd6098495c1d029f7515d47f4495638986addb40
Caroline Ninganga <carolninganga@gmail.com>
2020-05-20 14:14:47 -0400

    added CRUD

--- db/app.js
+++ db/app.js
@@ -57,11 +57,87 @@ connection.connect(function(err) {
           break;
 
         case UpdateOptions[4]:
-          connection.end();
+          UpdateEmployee();
+          
       }
     })
   }
+function employeeView() {
+  var query = "SELECT first_name, last_name, tile, salary FROM employee ";
+  query += "LEFT JOIN role";
+  query += "ON employee.role_id = role.id"
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    console.table(result)
+    start();
+  });
+};
 
+function departmentView() {
+  var query = "SELECT * from department";
+  connection.query(query, function(err, result) {
+    console.table(result)
+    start();
+  });
+};
+
+function roleView() {
+  var query = "SELECT * from role";
+  connection.query(query, function(err, result){
+    console.table("View Roles :" + result);
+    start();
+  });
+};
+
+function UpdateEmployee() {
+  connection.query("SELECT * FROM role", function(err, result) {
+
+    inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "Enter Employee's first_name:",
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message:"Enter Emplyoyee's last_name",
+      },
+      {
+        name: "role",
+        type:"list",
+        choices: function() {
+          var arr = [];
+          for(var i = 0; i < result.length; i++) {
+            arr.push(res[i].title);
+          }
+          return arr;
+        },
+        message: "What is the role ?"
+        
+      }])
+      .then(function(answer){
+        let employeeRoleID;
+        for(let j = 0; j < result.length; j++) {
+          if(result[j].title === answer.role) {
+            employeeRoleID = result[j].id;
+            console.log(employeeRoleID);
+          }
+        }
+
+        connection.query(
+          "INSERT INTO employee SET ?", {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: answer.employeeRoleID,
+          })
+      });
+  });
+};
+  function exitApp(){
+    connection.end();
+  }
 
   // * Add departments, roles, employees
 
